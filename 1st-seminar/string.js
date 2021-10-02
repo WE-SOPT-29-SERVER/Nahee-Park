dd9779ef333aa5050a120386980cb8a1e7e608ac
nahee-park <jessica396@ewhain.net>
2021-10-02 18:56:07 +0900

    [ADD] 4-4. data type - string

---
+++ 1st-seminar/string.js
@@ -0,0 +1,11 @@
+// string
+const str1 = "Just do it";
+const str2 = "Just do it";
+
+const name = "김우영";
+const backtickStr = `안녕하세요 ${name} 입니다`;
+
+// symbol
+/*
+안 쓸 거임 , 유일하고 변경할 수 없는 식별자 만들고 싶을 때 사용
+*/
